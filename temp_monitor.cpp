141149cf8d9ad3acb0ab693648d6095d2fe811b1
CristhoferAU <cristhofer.azofeifa@gmail.com>
2025-05-28 11:04:49 -0600

    Comments added

--- temp_monitor.cpp
+++ temp_monitor.cpp
@@ -9,10 +9,13 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+// Path para leer la temperatura actual del CPU
 #define TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"
-#define LOG_PATH "/tmp/temp_monitor.log"
-#define UMBRAL 90000 // 45 grados
+// Path para loguear las temperaturas leídas
+#define LOG_PATH "/home/cris/temp_monitor.log"
+#define UMBRAL 65000 // 65 grados
 
+// Función para leer la temperatura del CPU
 int leer_temperatura() {
     std::ifstream temp_file(TEMP_PATH);
     int temp = 0;
@@ -23,6 +26,7 @@ int leer_temperatura() {
     return temp;
 }
 
+// Función para escribir la temperatura del CPU en el log
 void log_temp(int temp) {
     std::ofstream log(LOG_PATH, std::ios::app);
     if (log.is_open()) {
@@ -32,14 +36,14 @@ void log_temp(int temp) {
 }
 
 int main() {
-    // Clean del log file al inicio
+    // Clean del log file al inicio de la ejecución
     std::ofstream log(LOG_PATH, std::ios::trunc);
     if (log.is_open()) log.close();
 
     openlog("temp_monitor", LOG_PID | LOG_CONS, LOG_DAEMON);
     syslog(LOG_NOTICE, "Iniciando temp_monitor");
 
-    // Detectar el usuario actual (usualmente el que ejecuta X o la sesión gráfica)
+    // Obtiene la información del usuario para la notificación
     uid_t uid = geteuid();
     struct passwd *pwd = getpwuid(uid);
     if (!pwd) {
@@ -52,13 +56,17 @@ int main() {
     while (true) {
         int temp = leer_temperatura();
 
+        // Escribe la temperatura actual en el log
         syslog(LOG_INFO, "Temperatura actual: %.2f °C", temp / 1000.0);
         log_temp(temp);
 
+        // Valida si la temperatura está por encima del Umbral
         if (temp >= UMBRAL) {
+            // Lanza log de un warnig si la temp es mal alta que Umbral
             syslog(LOG_WARNING, "⚠️ Temperatura alta: %.2f °C", temp / 1000.0);
-
+            // Verifica si está disponible notify-send para utilizarlo
             if (system("which notify-send > /dev/null 2>&1") == 0) {
+                // Lanza la notificación para el usuario
                 std::string cmd = "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=" + dbus_path +
                                   " notify-send '⚠️ Alerta de Temperatura' 'CPU supera los 45°C'";
                 system(cmd.c_str());
